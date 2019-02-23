73c6a34c4766c818e5b2c303b94273b41736c182
Matt Roth <roth_matthew@bah.com>
2019-02-23 10:30:04 -0500

    spring.io init

---
+++ src/main/java/hackathon9/ca_p3/CaP3Application.java
@@ -0,0 +1,13 @@
+package hackathon9.ca_p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaP3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaP3Application.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon9/ca_p3/CaP3ApplicationTests.java
@@ -0,0 +1,16 @@
+package hackathon9.ca_p3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaP3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
