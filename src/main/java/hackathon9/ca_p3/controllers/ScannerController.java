b633fc0303ffe2acee9b08c99c65a196cefa8328
Chad Howard <Chad.Howard@rsdcgroup.com>
2019-02-23 12:30:40 -0500

    Updated Controllers Added Item Repository

--- src/main/java/hackathon9/ca_p3/controllers/ScannerController.java
+++ src/main/java/hackathon9/ca_p3/controllers/ScannerController.java
@@ -1,11 +1,12 @@
 package hackathon9.ca_p3.controllers;
 
 import hackathon9.ca_p3.models.BarcodeInformation;
+import hackathon9.ca_p3.models.Item;
 import hackathon9.ca_p3.orchestration.ScannerOrchestrator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,16 @@ public class ScannerController {
     @Autowired
     public ScannerController(ScannerOrchestrator scannerOrchestrator) {
         this.scannerOrchestrator = scannerOrchestrator;
+    }
+
+    @PostMapping(value = "registerItem/{itemId}/{name}")
+    public void registerItem(@PathVariable String itemId, @PathVariable String name){
+        scannerOrchestrator.registerItem(itemId, name);
+    }
+
+    @GetMapping(value = "getItem/{itemId}")
+    public Item getItem(@PathVariable String itemId){
+        return scannerOrchestrator.getItem(itemId);
     }
 
     @PostMapping(value = "scanItem/{itemId}")
--- src/main/java/hackathon9/ca_p3/orchestration/ScannerOrchestrator.java
+++ src/main/java/hackathon9/ca_p3/orchestration/ScannerOrchestrator.java
@@ -1,10 +1,20 @@
 package hackathon9.ca_p3.orchestration;
 
 import hackathon9.ca_p3.models.BarcodeInformation;
+import hackathon9.ca_p3.models.Item;
+import hackathon9.ca_p3.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScannerOrchestrator {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ScannerOrchestrator(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     public String getScannedItem(String identifier){
         return "";
@@ -12,5 +22,16 @@ public class ScannerOrchestrator {
 
     public String saveScannedItem(BarcodeInformation barcodeInformation){
         return "";
+    }
+
+    public void registerItem(String itemId, String itemName){
+        Item item = new Item();
+        item.setItemId(itemId);
+        item.setItemName(itemName);
+        itemRepository.save(item);
+    }
+
+    public Item getItem(String itemId) {
+        return itemRepository.findById(itemId).orElse(null);
     }
 }
